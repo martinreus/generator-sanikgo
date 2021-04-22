c8dd12b3404c34da4138583bf12247862911513a
Martin Reus <martinreus@gmail.com>
2021-04-22 01:16:21 +0100

    add status to task

--- generators/task-runner/templates/task.go
+++ generators/task-runner/templates/task.go
@@ -2,9 +2,14 @@ package tasks
 
 import "context"
 
+type Status struct {
+
+}
+
 type Task interface {
 	// Start implementations should start a non blocking go-routine as soon as possible
 	Start(ctx context.Context) error
 	Stop(ctx context.Context) error
+	Status() Status
 	TaskName() string
 }
